7d475e88e70e260cef5e2b94a76273a1f9dc4997
autosapien <sapienauto@gmail.com>
2025-03-17 18:22:58 +0530

    Add option to specify replica_set

--- src/pytest_mock_resources/container/mongo.py
+++ src/pytest_mock_resources/container/mongo.py
@@ -19,20 +19,28 @@ class MongoConfig(DockerContainerConfig):
             Defaults to :code:`27017`.
         root_database (str): The name of the root mongo database to create.
             Defaults to :code:`"dev-mongo"`.
+        as_replica_set (bool): Whether to run the mongo container as a replica set.
+            Defaults to :code:`False`.
     """
 
     name = "mongo"
 
-    _fields: ClassVar[Iterable] = {"image", "host", "port", "ci_port", "root_database"}
+    _fields: ClassVar[Iterable] = {"image", "host", "port", "ci_port", "root_database", "as_mongo_replica_set",
+}
     _fields_defaults: ClassVar[dict] = {
         "image": "mongo:3.6",
         "port": 28017,
         "ci_port": 27017,
         "root_database": "dev-mongo",
+        "as_mongo_replica_set": False,
     }
 
     @fallback
     def root_database(self):
+        raise NotImplementedError()
+    
+    @fallback
+    def as_mongo_replica_set(self):
         raise NotImplementedError()
 
     def ports(self):
@@ -40,7 +48,10 @@ class MongoConfig(DockerContainerConfig):
 
     def check_fn(self):
         try:
-            client = pymongo.MongoClient(self.host, self.port, timeoutMS=1000)
+            if self.as_mongo_replica_set:
+                client = pymongo.MongoClient(self.host, self.port, timeoutMS=1000, directConnection=True)
+            else:
+                client = pymongo.MongoClient(self.host, self.port, timeoutMS=1000)
             db = client[self.root_database]
             db.command("ismaster")
         except pymongo.errors.PyMongoError:
